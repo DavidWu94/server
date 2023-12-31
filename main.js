f3b51082f6d1a8058f1a8b451324af2bd678db6c
DavidWu94 <154606523+DavidWu94@users.noreply.github.com>
2023-12-31 20:53:04 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// 處理POST請求
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+  // 接收POST請求的資料
+  const dataReceived = req.body;
+
+  // 在這裡處理收到的資料，進行必要的處理
+  // 這裡的例子是回傳一個固定的訊息和收到的資料
+  const responseData = {
+    message: '資料已收到！',
+    receivedData: dataReceived
+  };
+
+  // 回傳處理後的資料作為回應
+  res.json(responseData);
+});
+
+// 啟動伺服器
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
