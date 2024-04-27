9431cb33e545cde44f5576e72a469ca328bacf4a
Tobydog0501 <tobydog0501@gmail.com>
2024-04-27 17:34:10 +0800

    Login test successful

--- main.js
+++ main.js
@@ -2,38 +2,51 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser')
-
+const cors = require("cors");
+const sql = require("./sqlInteractions/sql.js")
+const a = new sql()
 // 處理POST請求
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,POST",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "Access-Control-Allow-Origin": "*"
+}))
 
-app.post('/submit', (req, res) => {
+
+app.post('/login', (req, res) => {
   // 接收POST請求的資料
   /**
    * @type {object}
    */
   const dataReceived = req.body;
+  // console.log(req.body)
 
   if(dataReceived["account"]==null || dataReceived["pwd"]==null){
-    res.json({
-      status:403,
-      message:`錯誤，並未輸入${dataReceived["account"]==null&&dataReceived["pwd"]==null ? "帳號及密碼":dataReceived["account"]==null ? "帳號":"密碼"}`
-    })
+    console.warn("Lack of info")
+    res.sendStatus(403);
+    // res.json({
+    //   status:403,
+    //   message:`錯誤，並未輸入${dataReceived["account"]==null&&dataReceived["pwd"]==null ? "帳號及密碼":dataReceived["account"]==null ? "帳號":"密碼"}`
+    // })
+    return;
   }
 
 
   // 回傳處理後的資料作為回應
-  console.log((req.body));
-  res.json({
-    status:200,
-    data:""
-  });
+  // console.log((req.body));
+  let b = a.login(dataReceived["account"],dataReceived["pwd"]);
+  res.json(b);
 });
 
 // 啟動伺服器
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
-  
+  // var a = new sql();
+  // a.connect();
 });
--- sqlInteractions/sql.js
+++ sqlInteractions/sql.js
@@ -1,27 +1,23 @@
-const mysql = require('mysql');
-const { deprecate } = require('util');
+// const mysql = require('mysql');
 
 class sql{
 
-    sql(){
-        this.con = mysql.createConnection({
-            host: "localhost",
-            user: process.env.USER_ID,
-            password: process.env.PWD
-        });
-
-        this.con.connect(function(err) {
-            if (err) throw err;
-            console.log("Connected!");
-        });
+    constructor(){
+        this.login_db = require('better-sqlite3')('./databases/loginDatabase.db');
+        this.sessionKeyDb = require('better-sqlite3')('./databases/sessionKey.db');
     }
+
 
     login(user,pwd){
-        
-    }
-
-    getUserType(user){
-
+        let realPwd = this.login_db.prepare(`SELECT * FROM userinfo WHERE id='${user}'`).all()[0];
+        if (realPwd==undefined) return {msg:"wrong account"}
+        // console.log(a)
+        if(pwd==realPwd["pwd"]){
+            // call for data
+            return {msg:"success",accountType:realPwd["type"]};
+        }else{
+            return {msg:"wrong pwd"};
+        }
     }
 
     getDayOffInfo(user){
