01cf9d2e9c4c76c80edc16b1e1a0fea0a7188190
Tobydog0501 <tobydog0501@gmail.com>
2024-08-09 13:06:15 +0800

    count fix

--- system/request.js
+++ system/request.js
@@ -38,6 +38,7 @@ module.exports = (sqlPlugin,log,mailer,req,res)=>{
       }
 
       let man = ["jeff@eucan.com.tw","catherine@eucan.com.tw"]
+      console.log(man[mgroup])
       mailer.send(man[mgroup],"請假審核要求",`您好，員工編號${account}於剛才發送請假要求。\n詳細內容請登入請假系統審核。\n\n<此信為系統自動發送，請勿回覆>`)
       res.json({
         "status":200,
