99f3f56b5f1d145ac75c7f7bc23364cdf5d6d3b3
Tobydog0501 <tobydog0501@gmail.com>
2025-02-11 19:47:43 +0800

    change sendfile to download

--- system/calendar.js
+++ system/calendar.js
@@ -34,7 +34,7 @@ module.exports = async (sqlPlugin,log,mailer,req,res)=>{
     log.logFormat(`${account} is requesting a calendar.`);
     await calen(year,month,sqlPlugin);
     log.logFormat(`Calendar has generated. Sending File /app/calendars/${year}-${month}calendar.xlsx...`);
-    res.sendFile(`/app/calendars/${year}-${month}calendar.xlsx`, (err) => {
+    res.download(`/app/calendars/${year}-${month}calendar.xlsx`, (err) => {
         if (err) {
             log.logFormat(`Error sending file /calendars/${year}-${month}calendar.xlsx: ` + err);
             res.sendStatus(500);
